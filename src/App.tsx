3d2d379a9bdfc5500f23c7e4e7d9a9e445204f97
Joel Hediger <joel.hediger@sonnenberg-baar.ch>
2025-10-15 14:56:35 +0200

    fix: GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Reindeer from './components/Reindeer';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -10,7 +10,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter basename="/vierkorken">
       <div className="d-flex flex-column vh-100">
         <Reindeer />
         <Header />
@@ -24,9 +24,8 @@ function App() {
         </main>
         <Footer />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
